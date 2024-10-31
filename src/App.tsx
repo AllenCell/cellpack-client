ae79249659e91f156d3a1322f33194a15c90fb04
Ruge Li <rugeli0605@gmail.com>
2024-10-31 14:05:46 -0700

    Co-authored-by: Alli <ascibisz@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,38 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(0);
-    const [text, setText] = useState("");
+    const [recipe, setRecipe] = useState("examples/recipes/v2/one_sphere.json");
+    const [jobId, setJobId] = useState("");
+    const submitRecipe = async () => {
+        console.log("recipe", recipe);
+        const request: RequestInfo = new Request(
+            'https://ng44ddk8v1.execute-api.us-west-2.amazonaws.com/testing/submit-packing?recipe=' + recipe,
+            {
+                method: 'POST',
+            }
+        )
+        const response = await fetch(request);
+        const data = await response.json();
+        console.log("data", data);
+        setJobId(data.jobId)
+        console.log("jobId", jobId);
+    }
     return (
-        <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
+        <div className="app">
+            <h1>Welcome to cellPACK</h1>
+            <div className="input-container">
                 <input
                     type="text"
-                    placeholder="Edit me!"
-                    onChange={(e) => setText(e.target.value)}
+                    placeholder="Enter a recipe"
+                    value={recipe}
+                    onChange={(e) => setRecipe(e.target.value)}
                 />
-                <p>{text} is the text you typed in the input box above. </p>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
+                <button onClick={submitRecipe}>
+                    Pack
+                </button>
             </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        </div>
     );
 }
 
