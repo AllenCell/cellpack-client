3a0f1e1871e34b9f4af6cec124f048838dc2dd36
ascibisz <alli.scibisz@alleninstitute.org>
2025-07-28 15:56:50 -0700

    try fixing github pages issue

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { decodeGitHubPagesUrl, isEncodedPathUrl, tryRemoveHashRouting } from "./utils/gh_routing";
+
+// Decode URL path if it was encoded for GitHub pages or uses hash routing.
+const locationUrl = new URL(window.location.toString());
+if (locationUrl.hash !== "" || isEncodedPathUrl(locationUrl)) {
+  const decodedUrl = tryRemoveHashRouting(decodeGitHubPagesUrl(locationUrl));
+  const newRelativePath = decodedUrl.pathname + decodedUrl.search + decodedUrl.hash;
+  console.log("Redirecting to " + newRelativePath);
+  // Replaces the query string path with the original path now that the
+  // single-page app has loaded. This lets routing work as normal below.
+  window.history.replaceState(null, "", newRelativePath);
+}
 
 const router = createBrowserRouter(
   [
@@ -11,6 +23,7 @@ const router = createBrowserRouter(
       element: <App/>,
     },
   ],
+  { basename: import.meta.env.BASE_URL }
 );
 
 createRoot(document.getElementById('root')!).render(
