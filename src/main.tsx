caeb0a650875c78fea3c5504f94140e995497b86
ascibisz <alli.scibisz@alleninstitute.org>
2025-07-28 16:07:47 -0700

    add support for single page applications for gh pages

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,18 @@ import { PageRoutes } from './constants/routes'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import './index.css'
 import App from './App.tsx'
+import { decodeGitHubPagesUrl, isEncodedPathUrl, tryRemoveHashRouting } from "./utils/gh_routing";
+
+// Decode URL path if it was encoded for GitHub pages or uses hash routing.
+const locationUrl = new URL(window.location.toString());
+if (locationUrl.hash !== "" || isEncodedPathUrl(locationUrl)) {
+  const decodedUrl = tryRemoveHashRouting(decodeGitHubPagesUrl(locationUrl));
+  const newRelativePath = decodedUrl.pathname + decodedUrl.search + decodedUrl.hash;
+  console.log("Redirecting to " + newRelativePath);
+  // Replaces the query string path with the original path now that the
+  // single-page app has loaded. This lets routing work as normal below.
+  window.history.replaceState(null, "", newRelativePath);
+}
 
 const router = createBrowserRouter(
   [
