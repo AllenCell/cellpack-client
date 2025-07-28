f4ca66ca4272334ddf82c549a5a4f418614f76cd
ascibisz <alli.scibisz@alleninstitute.org>
2025-07-28 15:02:56 -0700

    remove router provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-// const router = createBrowserRouter(
-//   [
-//     {
-//       path: "/",
-//       element: <App/>,
-//     },
-//   ],
-// );
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App/>,
+    },
+  ],
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    {/* <RouterProvider router={router} /> */}
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </StrictMode>,
 )
