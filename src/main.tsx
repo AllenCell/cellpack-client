61b965e01a53a4631744816c89c97850d390389f
ascibisz <alli.scibisz@alleninstitute.org>
2025-07-28 15:01:01 -0700

    fix typo

--- src/main.tsx
+++ src/main.tsx
@@ -15,7 +15,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App
+    <App />
     {/* <RouterProvider router={router} /> */}
   </StrictMode>,
 )
