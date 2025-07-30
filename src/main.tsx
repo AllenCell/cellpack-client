6b635767a4886cae4cd15a24ca01002392c07691
Ruge Li <91452427+rugeli@users.noreply.github.com>
2025-07-30 15:21:43 -0700

    Initial routes structure  (#45)
    * add react router dom
    
    * basic routes
    
    * rename viewer to pack
    
    * add support for single page applications for gh pages (#50)
    
    * add support for single page applications for gh pages
    
    * set basename
    
    ---------
    
    Co-authored-by: Alli <111383930+ascibisz@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,39 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { LandingPage } from './routes'
+import { PageRoutes } from './constants/routes'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import './index.css'
 import App from './App.tsx'
+import { decodeGitHubPagesUrl, isEncodedPathUrl, tryRemoveHashRouting } from "./utils/gh_routing";
+
+// Decode URL path if it was encoded for GitHub pages or uses hash routing.
+const locationUrl = new URL(window.location.toString());
+if (locationUrl.hash !== "" || isEncodedPathUrl(locationUrl)) {
+  const decodedUrl = tryRemoveHashRouting(decodeGitHubPagesUrl(locationUrl));
+  const newRelativePath = decodedUrl.pathname + decodedUrl.search + decodedUrl.hash;
+  console.log("Redirecting to " + newRelativePath);
+  // Replaces the query string path with the original path now that the
+  // single-page app has loaded. This lets routing work as normal below.
+  window.history.replaceState(null, "", newRelativePath);
+}
+
+const router = createBrowserRouter(
+  [
+    {
+      path: PageRoutes.LANDING_PAGE,
+      element: <LandingPage />,
+    },
+    {
+      path: PageRoutes.PACKING_PAGE,
+      element: <App/>,
+    },
+  ],
+  { basename: import.meta.env.BASE_URL }
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
