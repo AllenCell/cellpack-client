60fa09daa203c9daac559e82b69b470f707f5ad7
meganrm <meganrm@gmail.com>
2025-10-16 16:32:17 -0700

    main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import { useState } from "react";
+import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import { Layout, Typography } from "antd";
-import { getResultPath, getDocById, getJobStatus, addRecipe } from "./utils/firebase";
-import { getFirebaseRecipe, jsonToString } from "./utils/recipeLoader";
-import { getSubmitPackingUrl, JOB_STATUS } from "./constants/aws";
-import { FIRESTORE_COLLECTIONS, FIRESTORE_FIELDS } from "./constants/firebase";
-import { SIMULARIUM_EMBED_URL } from "./constants/urls";
-import PackingInput from "./components/PackingInput";
-import Viewer from "./components/Viewer";
-import ErrorLogs from "./components/ErrorLogs";
-import StatusBar from "./components/StatusBar";
-import "./App.css";
+import { Layout, Typography } from 'antd';
+import {
+    getResultPath,
+    getDocById,
+    getJobStatus,
+    addRecipe,
+} from './utils/firebase';
+import { getFirebaseRecipe, jsonToString } from './utils/recipeLoader';
+import { getSubmitPackingUrl, JOB_STATUS } from './constants/aws';
+import { FIRESTORE_COLLECTIONS, FIRESTORE_FIELDS } from './constants/firebase';
+import { SIMULARIUM_EMBED_URL } from './constants/urls';
+import PackingInput from './components/PackingInput';
+import Viewer from './components/Viewer';
+import ErrorLogs from './components/ErrorLogs';
+import StatusBar from './components/StatusBar';
+import './App.css';
+import Sider from 'antd/es/layout/Sider';
+import { Footer } from 'antd/es/layout/layout';
 
 const { Header, Content } = Layout;
 const { Link } = Typography;
 
 function App() {
-    const [jobId, setJobId] = useState("");
-    const [jobStatus, setJobStatus] = useState("");
-    const [jobLogs, setJobLogs] = useState<string>("");
-    const [resultUrl, setResultUrl] = useState<string>("");
+    const [jobId, setJobId] = useState('');
+    const [jobStatus, setJobStatus] = useState('');
+    const [jobLogs, setJobLogs] = useState<string>('');
+    const [resultUrl, setResultUrl] = useState<string>('');
     const [runTime, setRunTime] = useState<number>(0);
 
     let start = 0;
@@ -28,12 +35,19 @@ function App() {
         return new Promise((resolve) => setTimeout(resolve, ms));
     }
 
-    const recipeHasChanged = async (recipeId: string, recipeString: string): Promise<boolean> => {
+    const recipeHasChanged = async (
+        recipeId: string,
+        recipeString: string
+    ): Promise<boolean> => {
         const originalRecipe = await getFirebaseRecipe(recipeId);
         return !(jsonToString(originalRecipe) == recipeString);
-    }
+    };
 
-    const recipeToFirebase = (recipe: string, path: string, id: string): object => {
+    const recipeToFirebase = (
+        recipe: string,
+        path: string,
+        id: string
+    ): object => {
         const recipeJson = JSON.parse(recipe);
         if (recipeJson.bounding_box) {
             const flattened_array = Object.assign({}, recipeJson.bounding_box);
@@ -43,18 +57,31 @@ function App() {
         recipeJson[FIRESTORE_FIELDS.NAME] = id;
         recipeJson[FIRESTORE_FIELDS.TIMESTAMP] = Date.now();
         return recipeJson;
-    }
+    };
 
-    const submitRecipe = async (recipeId: string, configId: string, recipeString: string) => {
-        setResultUrl("");
+    const submitRecipe = async (
+        recipeId: string,
+        configId: string,
+        recipeString: string
+    ) => {
+        setResultUrl('');
         setRunTime(0);
-        let firebaseRecipe = "firebase:recipes/" + recipeId;
-        const firebaseConfig = configId ? "firebase:configs/" + configId : undefined;
-        const recipeChanged: boolean = await recipeHasChanged(recipeId, recipeString);
+        let firebaseRecipe = 'firebase:recipes/' + recipeId;
+        const firebaseConfig = configId
+            ? 'firebase:configs/' + configId
+            : undefined;
+        const recipeChanged: boolean = await recipeHasChanged(
+            recipeId,
+            recipeString
+        );
         if (recipeChanged) {
             const recipeId = uuidv4();
-            firebaseRecipe = "firebase:recipes_edited/" + recipeId;
-            const recipeJson = recipeToFirebase(recipeString, firebaseRecipe, recipeId);
+            firebaseRecipe = 'firebase:recipes_edited/' + recipeId;
+            const recipeJson = recipeToFirebase(
+                recipeString,
+                firebaseRecipe,
+                recipeId
+            );
             try {
                 await addRecipe(recipeId, recipeJson);
             } catch (e) {
@@ -62,10 +89,9 @@ function App() {
                 setJobLogs(String(e));
                 return;
             }
-
         }
         const url = getSubmitPackingUrl(firebaseRecipe, firebaseConfig);
-        const request: RequestInfo = new Request(url, { method: "POST" });
+        const request: RequestInfo = new Request(url, { method: 'POST' });
         start = Date.now();
         const response = await fetch(request);
         setJobStatus(JOB_STATUS.SUBMITTED);
@@ -80,15 +106,23 @@ function App() {
         }
     };
 
-    const startPacking = async (recipeId: string, configId: string, recipeString: string) => {
-        await submitRecipe(recipeId, configId, recipeString)
-            .then((jobIdFromSubmit) => checkStatus(jobIdFromSubmit));
-    }
+    const startPacking = async (
+        recipeId: string,
+        configId: string,
+        recipeString: string
+    ) => {
+        await submitRecipe(recipeId, configId, recipeString).then(
+            (jobIdFromSubmit) => checkStatus(jobIdFromSubmit)
+        );
+    };
 
     const checkStatus = async (jobIdFromSubmit: string) => {
         const id = jobIdFromSubmit || jobId;
         let localJobStatus = await getJobStatus(id);
-        while (localJobStatus !== JOB_STATUS.DONE && localJobStatus !== JOB_STATUS.FAILED) {
+        while (
+            localJobStatus !== JOB_STATUS.DONE &&
+            localJobStatus !== JOB_STATUS.FAILED
+        ) {
             await sleep(500);
             const newJobStatus = await getJobStatus(id);
             if (localJobStatus !== newJobStatus) {
@@ -113,24 +147,47 @@ function App() {
 
     const getLogs = async (jobIdFromSubmit?: string) => {
         const id = jobIdFromSubmit || jobId;
-        const logStr: string = await getDocById(FIRESTORE_COLLECTIONS.JOB_STATUS, id);
+        const logStr: string = await getDocById(
+            FIRESTORE_COLLECTIONS.JOB_STATUS,
+            id
+        );
         setJobLogs(logStr);
     };
     const showLogs = jobStatus == JOB_STATUS.FAILED;
 
     return (
-        <div className="app-container">
-            <Header className="header" style={{ justifyContent: "space-between" }}>
-                <h2 className="header-title">cellPACK demo</h2>
-                <Link href="https://github.com/mesoscope/cellpack" className="header-link">GitHub</Link>
+        <Layout className='app-container'>
+            <Header
+                className='header'
+                style={{ justifyContent: 'space-between' }}
+            >
+                <h2 className='header-title'>cellPACK demo</h2>
+                <Link
+                    href='https://github.com/mesoscope/cellpack'
+                    className='header-link'
+                >
+                    GitHub
+                </Link>
             </Header>
-            <Content className="content-container">
-                <PackingInput startPacking={startPacking} />
-                {jobStatus && <StatusBar jobStatus={jobStatus} runTime={runTime} jobId={jobId} />}
-                {showLogs && <ErrorLogs errorLogs={jobLogs} getLogs={getLogs} />}
-            </Content>
-            {resultUrl && <Viewer resultUrl={resultUrl} />}
-        </div>
+            <Layout>
+                <Sider width='35%' theme='light' className='sider'>
+                    <PackingInput startPacking={startPacking} />
+                </Sider>
+                <Content className='content-container'>
+                    <Viewer resultUrl={resultUrl} />
+                    {/* {showLogs && (
+                        <ErrorLogs errorLogs={jobLogs} getLogs={getLogs} />
+                    )} */}
+                </Content>
+            </Layout>
+            <Footer className='footer'>
+                <StatusBar
+                    jobStatus={jobStatus}
+                    runTime={runTime}
+                    jobId={jobId}
+                />
+            </Footer>
+        </Layout>
     );
 }
 
