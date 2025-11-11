b8b29482f2b88d8e1ee9d5b533d26c16738b6531
Joe Heffernan <interim17@gmail.com>
2025-11-10 19:23:46 -0800

    add loading overlay to viewer

--- src/components/Viewer/index.tsx
+++ src/components/Viewer/index.tsx
@@ -1,15 +1,35 @@
+import { LoadingOutlined } from "@ant-design/icons";
 import { SIMULARIUM_EMBED_URL } from "../../constants/urls";
-import { useResultUrl } from "../../state/store";
+import { useCurrentRecipeObject, useIsPacking, useResultUrl } from "../../state/store";
 import "./style.css";
 
 const Viewer = (): JSX.Element => {
     const resultUrl = useResultUrl();
+    const recipeObj = useCurrentRecipeObject();
+    const isPacking = useIsPacking();
+
+    const overlayText = isPacking
+        ? "Running..."
+        : !recipeObj
+        ? "Loading..."
+        : "";
+
+    const showOverlay = !recipeObj || isPacking;
+
     return (
         <div className="viewer-container">
             <iframe
                 className="simularium-embed"
                 src={`${SIMULARIUM_EMBED_URL}${resultUrl}`}
             />
+            {showOverlay && (
+                <div className="viewer-overlay">
+                    <div className="overlay-content">
+                        <LoadingOutlined />
+                        <p>{overlayText}</p>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
