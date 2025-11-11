2330aef9dd8f5d2806e13ce1bff6843fe92fd946
Joe Heffernan <interim17@gmail.com>
2025-11-10 16:29:25 -0800

    use viewer overlay when loading and packing

--- src/components/Viewer/index.tsx
+++ src/components/Viewer/index.tsx
@@ -1,15 +1,35 @@
+import { LoadingOutlined } from "@ant-design/icons";
 import { SIMULARIUM_EMBED_URL } from "../../constants/urls";
-import { useResultUrl } from "../../state/store";
+import { useIsLoading, useIsPacking, useResultUrl } from "../../state/store";
 import "./style.css";
 
 const Viewer = (): JSX.Element => {
     const resultUrl = useResultUrl();
+    const isLoading = useIsLoading();
+    const isPacking = useIsPacking();
+
+    const overlayText = isPacking
+        ? "Running..."
+        : isLoading
+        ? "Loading..."
+        : "";
+
+    const showOverlay = isLoading || isPacking;
+
     return (
         <div className="viewer-container">
             <iframe
                 className="simularium-embed"
                 src={`${SIMULARIUM_EMBED_URL}${resultUrl}`}
             />
+            {showOverlay && (
+                <div className="viewer-overlay">
+                    <div className="overlay-content">
+                        <LoadingOutlined />
+                        <p>{overlayText}</p>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
