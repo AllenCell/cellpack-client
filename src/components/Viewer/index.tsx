aee1ec0dd21b438c661ae8f01e1e6533fc50dc37
Joe Heffernan <interim17@gmail.com>
2025-11-18 12:59:58 -0800

    add viewer overlay for loading, packing and modified states

--- src/components/Viewer/index.tsx
+++ src/components/Viewer/index.tsx
@@ -1,15 +1,43 @@
+import { LoadingOutlined } from "@ant-design/icons";
 import { SIMULARIUM_EMBED_URL } from "../../constants/urls";
-import { useResultUrl } from "../../state/store";
+import { useIsLoading, useIsModified, useIsPacking, useResultUrl } from "../../state/store";
 import "./style.css";
 
 const Viewer = (): JSX.Element => {
     const resultUrl = useResultUrl();
+    const isLoading = useIsLoading();
+    const isPacking = useIsPacking();
+    const isModified = useIsModified();
+
+    const overlayText = isPacking
+        ? "Running..."
+        : isLoading
+        ? "Loading..."
+        : isModified
+        ? "Re-run packing to view result"
+        : "";
+    
+    const activeState = isLoading || isPacking;
+    const showOverlay = activeState || isModified;
+
     return (
         <div className="viewer-container">
             <iframe
                 className="simularium-embed"
                 src={`${SIMULARIUM_EMBED_URL}${resultUrl}`}
             />
+            {showOverlay && (
+                <div className="viewer-overlay">
+                    <div
+                        className={`overlay-content ${
+                            activeState ? "active" : ""
+                        }`}
+                    >
+                        {activeState && <LoadingOutlined />}
+                        <p>{overlayText}</p>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
