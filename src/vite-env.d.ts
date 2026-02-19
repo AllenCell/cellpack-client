99b0799ae108cbc802c9c1ce457d83bbcc1d7535
Ruge Li <rugeli0605@gmail.com>
2026-02-19 12:57:59 -0800

    uninstall svg plugin

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,1 @@
 /// <reference types="vite/client" />
-// adds the types for the svgr plugin, so typescript know they resolve to React components instead of url strings
-/// <reference types="vite-plugin-svgr/client" />
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
 import { resolve } from 'path';
 
 // https://vite.dev/config/
@@ -13,5 +12,5 @@ export default defineConfig({
       }
     }
   },
-  plugins: [react(), svgr()],
+  plugins: [react()],
 })
