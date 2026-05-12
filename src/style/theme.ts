dd74bc44a0fa841e44ce0751305270f5e59cb154
Joe Heffernan <interim17@gmail.com>
2026-05-11 17:56:59 -0700

    change styling on slider, tabs, and tooltip

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -3,6 +3,40 @@ import { theme } from "antd";
 
 const commonComponents = {
     Button: { controlHeight: 40 },
+    Slider: {
+        trackBg: '#7AA4D7',
+        trackHoverBg: '#5F92CE',
+        handleColor: '#7AA4D7',
+        handleActiveColor: '#3378C4',
+        handleColorDisabled: '#C6D2DE',
+        trackBgDisabled: '#C6D2DE',
+    },
+    Tabs: {
+        inkBarColor: '#468ADE',
+        itemHoverColor: '#6BA0E5',
+        titleFontSize: 18,
+    },
+    Tooltip: {
+        colorTextLightSolid: '#000000',
+        colorBgSpotlight: '#cbe3ff',
+        boxShadowSecondary: '4px 3px 10px 0px #1F1F1FCC',
+    }
+};
+
+const lightComponents = {
+    ...commonComponents,
+    Tabs: {
+        ...commonComponents.Tabs,
+        colorPrimary: '#000000',
+    },
+};
+
+const darkComponents = {
+    ...commonComponents,
+    Tabs: {
+        ...commonComponents.Tabs,
+        colorPrimary: '#ffffff',
+    },
 };
 
 export const lightTheme: ThemeConfig = {
@@ -15,7 +49,7 @@ export const lightTheme: ThemeConfig = {
         colorTextBase: "#213547",
     },
     components: {
-        ...commonComponents,
+        ...lightComponents,
         Layout: {
             headerBg: "#ffffff",
             bodyBg: "#ffffff",
@@ -33,7 +67,7 @@ export const darkTheme: ThemeConfig = {
         colorTextBase: "rgba(255,255,255,0.87)",
     },
     components: {
-        ...commonComponents,
+        ...darkComponents,
         Layout: {
             headerBg: "#0f1115",
             bodyBg: "#0f1115",
